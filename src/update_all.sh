5856598347f4d1245260f5feadf9a636e8399717
Benoît Legat <benoit.legat@gmail.com>
2012-07-17 13:48:50 +0200

    update_all.sh added, useful when changed lib.tex

---
+++ src/update_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update all the pdf (usefull when updating lib.tex)
+
+arr=("elec math meca" "chimie elec math meca philo")
+for i in ${!arr[*]}; do
+	let "q = i + 1"
+	cd "q$q"
+	for j in ${arr[$i]}; do
+		cd $j
+		make all
+		touch "$j.tex"
+		make addpdf
+		cd ..
+	done
+	cd ..
+done
