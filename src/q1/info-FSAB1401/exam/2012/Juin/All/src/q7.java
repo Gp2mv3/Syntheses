021b378fb06dfcd7bc46d5de7d9c807d5f16d1ea
Mattéo Couplet <matteo.couplet@gmail.com>
2016-01-12 21:46:13 +0100

    info-FSAB1401-exam-2012-Juin_All: meilleur réponse à Q7

--- src/q1/info-FSAB1401/exam/2012/Juin/All/src/q7.java
+++ src/q1/info-FSAB1401/exam/2012/Juin/All/src/q7.java
@@ -2,14 +2,8 @@ public int NAuthors() {
     LinkedList<String> authors = new LinkedList<String>();
     for(int i = 0; i < contents.length; i++) {
         String a = contents[i].getAuthor();
-        boolean inList = false;
-        Iterator<String> it = authors.iterator();
-        while(it.hasNext()) {
-            String b = it.next();
-            if(a.equals(b))
-                inList = true;
-        }
-        if(!inList) authors.add(a);
+        if(!authors.contains(a))
+            authors.add(a);
     }
     return authors.size();
 }
