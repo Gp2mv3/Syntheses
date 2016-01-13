d6a0952b59741e9bf10d623c70bc0bad695c12e4
Mattéo Couplet <matteo.couplet@gmail.com>
2016-01-13 23:17:19 +0100

    juin 2012: erreur q4

--- src/q1/info-FSAB1401/exam/2012/Juin/All/src/q4.java
+++ src/q1/info-FSAB1401/exam/2012/Juin/All/src/q4.java
@@ -1,12 +1,12 @@
 public void addBefore(Song songin, Song songnew) {
     // on verifie que le premier element n est pas la chanson recherchee
-    if(head.data.equals(songin)) {
+    if(head.data == songin) {
         Node newHead = new Node(songnew, head);
         head = newHead;
     } else {
         runner = head;
         while(runner != tail) {
-            if(runner.next.data.equals(songin)) {
+            if(runner.next.data == songin) {
                 Node newNode = new Node(songnew, runner.next);
                 runner.next = newNode;
                 return;
