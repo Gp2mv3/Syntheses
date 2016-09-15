407a8f7916db09580442f85d263dc54472b7ce56
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2016-09-16 00:01:32 +0200

    Corrects tabs in q5, add quotes for String in q3

--- src/q1/info-FSAB1401/exam/2012/Juin/All/src/q5.java
+++ src/q1/info-FSAB1401/exam/2012/Juin/All/src/q5.java
@@ -1,23 +1,23 @@
 private class JukeBoxListener implements PlayerListener {
-    /**
-     * @pre -
-     * @post Crée un JukeBoxListener
-     */
-    public JukeBoxListener() {}
+	/**
+	 * @pre -
+	 * @post Crée un JukeBoxListener
+	 */
+	public JukeBoxListener() {}
 
-    /**
-     * Cf interface PlayerListener
-     * @pre song != null
-     * @post une nouvelle chanson est sur écoute.
-     */
-    public void endOfSong(Song song) {
-        if(list.isEmpty())
-	    return;
-        while(player.isPlaying()) {
-	    // ne fait rien, attend la fin
+	/**
+	 * Cf interface PlayerListener
+	 * @pre song != null
+	 * @post une nouvelle chanson est sur écoute.
+	 */
+	public void endOfSong(Song song) {
+		if(list.isEmpty())
+			return;
+		while(player.isPlaying()) {
+			// ne fait rien, attend la fin
+		}
+		player.play(list.dequeue());
 	}
-        player.play(list.dequeue());
-    }
 }
 
 // utilité du while, sachant que normalement quand endOfSong est appelé, il n'y a plus de chanson en cours ?
