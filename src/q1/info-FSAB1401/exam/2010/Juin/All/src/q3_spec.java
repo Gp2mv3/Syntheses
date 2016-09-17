400315e1e5211aca8cf328db99a162e8f0bc6f94
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2016-09-17 18:34:33 +0200

    Corrected specifications of info1 exam 2010

--- src/q1/info-FSAB1401/exam/2010/Juin/All/src/q3_spec.java
+++ src/q1/info-FSAB1401/exam/2010/Juin/All/src/q3_spec.java
@@ -1,6 +1,6 @@
 /**
- * @pre  au moins une mesure valide a ete stockee
- * @post retourne la temperature moyenne des mesures valides stockees dans la station meteo
+ * @pre  au moins une mesure valide a été stockée
+ * @post retourne la temperature moyenne des mesures valides stockées dans la station météo
  */
 public double moyenneTemperature()
 
