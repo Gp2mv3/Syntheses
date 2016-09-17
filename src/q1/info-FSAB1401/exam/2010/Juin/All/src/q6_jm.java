8d2871fcc50f109df274599af630ded86776f9fa
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2016-09-17 18:31:13 +0200

    Clarifications, other solution q8, suggestion q6

---
+++ src/q1/info-FSAB1401/exam/2010/Juin/All/src/q6_jm.java
@@ -0,0 +1,23 @@
+public void sauvegarde(String fileName) {
+	PrintWriter pw;
+	try {
+		pw = new PrintWriter(new FileWriter(fileName)); // throws IOException
+	} catch(IOException e) {
+		System.err.println("Erreur lors de l'ouverture du fichier");
+		System.exit(-1);
+	}
+	// écrivons ; PrintWriter ne jette pas d'exception lors de l'écriture
+	for(MesureEtendue m : super.mesures)
+		pw.printf("%d:%f\n", m.getPression(), m.getTemperature());
+	
+	// Il est temps de vérifier qu'il n'y a pas eu d'erreur
+	boolean erreurs = pw.checkError();
+	pw.close();
+	if (erreurs) {
+		System.err.println("Erreur(s) lors de l'écriture du fichier");
+		System.exit(-2);
+	}
+}
+
+// pas sûr de l'utilité du super, vu qu'il n'y a rien d'overridé
+// on aurait pu utiliser println directement, si on avait bien défini toString() ;-)
---
+++ src/q1/info-FSAB1401/exam/2010/Juin/All/src/q8_alter.java
@@ -0,0 +1,19 @@
+/* Dans la suite, on suppose que la liste est bien construite, sans erreurs sur les pointeurs,
+ * et que n appartient bien à la liste. */
+private void remove(Node n) {
+	if(n.previous == null) // n au début de la liste; équivalent à front==n
+		front = n.next; // donc on avance le début
+	else // je n'y suis pas ; n a donc un précédent
+		n.previous.next = n.next;
+	
+	if(n.next == null) // en fin de liste
+		back = n.previous;
+	else // n a un suivant
+		n.next.previous = n.previous;
+	
+	// Le cas où n est seul dans la liste implique n.next=n.previous=null, et après exécution front=back=null
+	
+	// safety : on met les références à null, pour faciliter le travail de la JVM
+	n.next = n.previous = null;
+}
+
