c19009004855204550272bad79fa841edcd1d0d1
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-06-30 13:46:56 +0000

    now changing dir

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-Diff=$(git diff --name-only HEAD^ HEAD)
-Pattern=".tex"
-# We can change the pattern easily
 
-echo $Diff
+# Input string
+input="path/to/location/file.tex"
+pattern=".tex"
 
-for arg in "$Diff"; do
-    echo $arg
-    if echo "$arg" | grep -q "$Pattern"; then
-        echo "$arg"
-        pdflatex $arg
-    fi
+# Set the IFS to "/"
+IFS="/"
+
+# Read the input string into an array
+read -ra parts <<< "$input"
+
+# Restore the default IFS
+IFS=" "
+
+# Print the array elements
+for part in "${parts[@]}"; do
+  if echo "$part" | grep -q "$pattern"; then
+    # Print the matched argument
+    pdflatex "$part"
+  fi
+  cd "$part"
+  ls
 done
